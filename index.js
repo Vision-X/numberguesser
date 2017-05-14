3773260f31168d5298d4b54ab129efca8e78aca6
Mike Marlow <michael.s.marlow@gmail.com>
2017-05-14 14:47:11 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var userInput = document.getElementById("guess-form");
+var displayGuess = document.getElementById('display-guess');
+var guessButton = document.querySelector('#guess-button');
+var clearButton= document.getElementById('clear-button');
+
+var randomNum = Math.floor(Math.random() * (100) +1);
+
+//guess button functionality
+guessButton.addEventListener('click', function() {
+  var input = userInput.value;
+  console.log(input)
+  displayGuess.innerText=input;
+  userInput.value='';
+  console.log(randomNum);
+})
+
+//clear guess button
+clearButton.addEventListener('click', function() {
+  userInput.value='';
+})
+
+
+//Enable Buttons when the INPUT field is typed in //
+function buttonsOn() {
+  document.getElementById('guess-button').removeAttribute("disabled");
+  document.getElementById('clear-button').removeAttribute("disabled");
+  document.getElementById('reset-button').removeAttribute("disabled");
+}
+//Place user inout into guess field after ENTER pressed //
+guessButton.onClick = userGuess;
+function userGuess () {
+
+    var guessNumber = parseInt
+}
+
+  var x = document.getElementById("guess-form").value;
+  document.getElementById("display-guess").innerHTML = x;
