8373dc7c31c085999ea65bb14aa185f7030634e5
DanGrund <hello@dangrund.com>
2017-05-14 15:14:00 -0600

    finish guess logic basics, working on dan's machine. no outlets at crema

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ var userInput = document.getElementById("guess-form");
 var displayGuess = document.getElementById('display-guess');
 var guessButton = document.querySelector('#guess-button');
 var clearButton= document.getElementById('clear-button');
+var displayHighLow = document.getElementById('display-high-low');
 
-var randomNum = Math.floor(Math.random() * (100) +1);
+var randomNum = randomNumber();
+var min = 0
+var max = 100
 
 //guess button functionality
 guessButton.addEventListener('click', function() {
   var input = userInput.value;
-  console.log(input)
   displayGuess.innerText=input;
   userInput.value='';
-  console.log(randomNum);
+  userGuess(input)
 })
 
-//clear guess button
+//clear button
 clearButton.addEventListener('click', function() {
   userInput.value='';
 })
-
 
 //Enable Buttons when the INPUT field is typed in //
 function buttonsOn() {
@@ -26,19 +27,32 @@ function buttonsOn() {
   document.getElementById('clear-button').removeAttribute("disabled");
   document.getElementById('reset-button').removeAttribute("disabled");
 }
-//Place user inout into guess field after ENTER pressed //
-function userGuess () {
-  if (userInput < randomNum) {
-    //too low
-  }
-  else if (userInput > randomNum) {
-    //too high
-  } else {
-    //correct!!
-  }
 
-    var guessNumber = parseInt
+//Place user inout into guess field after ENTER pressed //
+function userGuess (userGuess) {
+  console.log(randomNum);
+  if (userGuess < randomNum) {
+    //too low
+    displayHighLow.innerText='Lowwwww';
+  }
+  else if (userGuess > randomNum) {
+    //too high
+    displayHighLow.innerText='Highhhh';
+  } else {
+  //correct!!
+    displayHighLow.innerText='Winnerrrrr';
+
+    randomNum = randomNumber();
+  }
 }
 
-  var x = document.getElementById("guess-form").value;
-  document.getElementById("display-guess").innerHTML = x;
+//reset button functionality
+
+//random Number generator function
+function randomNumber () {
+  return Math.floor(Math.random() * (max-min) +1);
+}
+
+//function to update min
+
+//function to update max
